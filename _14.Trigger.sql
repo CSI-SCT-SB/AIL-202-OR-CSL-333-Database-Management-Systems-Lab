00b13c21eeb8c3be0de9b1560d8417c700e25ffe
HARIKRISHNAN U S <108210868+HKRcodes@users.noreply.github.com>
2022-10-29 22:54:46 +0530

    Add files via upload

---
+++ _14.Trigger.sql
@@ -0,0 +1,12 @@
+CREATE OR REPLACE TRIGGER restrict Book
+BEFORE INSERT ON BOOK
+FOR EACH ROW
+DECLARE
+   avgc float;
+BEGIN
+   select avg(cost) INTO avgc from book;
+   IF:new.cost<=avgc THEN
+      raise_application_error(-20000,'Cost of book cannot be less than average cost of books');
+   END IF;
+END;
+/
